dbea08ed8408254b3608ffd2262f3ebf677ebad2
imnotus <39824578+imnotus@users.noreply.github.com>
2023-04-18 19:26:28 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ const double end_time = 1000;
 constexpr double PI = 3.14159265358979323846264338;
 const double theta = pow(10, 0.4);
 //const double delta = 2.0 / pass_loss_exponent;
-const double lambda_BS = 1.0;
+const double lambda_BS = 1.;
+const double POWER = 100;
 
 //出力ファイルを開く
 ofstream outputfile;
@@ -28,6 +29,9 @@ struct terminal {
     //double gain;
     double coef;
     bool state; //True: in , False: out
+    bool operator<( const terminal& right ) const {
+            return coef == right.coef ? true : coef < right.coef;
+    }
 };
 
 
@@ -51,11 +55,10 @@ double my_rand(double Min, double Max) {
 //円形領域内の座標をランダムに取得
 pair<double, double> coordinate() {
     //init_genrand(i);
-    double r = radius * sqrt(urand());
-    double theta = PI * (2. * urand() - 1.);
-    while (1) {
-        if (theta > -PI && theta < PI) break;
-        theta = my_rand(-PI, PI);
+    double r = radius * sqrt(urand()); //[0, 1)
+    double theta = PI * (2. * urand() - 1.); //(-1, 1)
+    while (theta < -PI || PI < theta) {
+        theta = PI * (2. * urand() - 1.);
     }
     double x = r * cos(theta);
     double y = r * sin(theta);
@@ -129,65 +132,6 @@ double thp_theo(double lambda_IoT, double lambda_BS, double alpha) {
 
 
 
-
-void sim_pr(double lambda_IoT, double alpha) {
-    double success = 0;
-    cout << "pass loss exp : " << alpha << endl;
-    
-//    bool p_flag[4] = {true, true, true, true};
-//    cout << "Progress is 0%";
-    for (int i = 0; i < end_time; i++) {
-//        random_device rd;
-//        init_genrand(rd());
-        int num_IoT = poisson_dist(4 * radius * radius * lambda_IoT);
-        int num_BS = poisson_dist(4 * radius * radius * lambda_BS);
-        //BSの座標を設定
-        vector<pair<double, double>> BS_pos(num_BS);
-        pair<double, double> origin = make_pair(0, 0);
-        int index = 0;
-        double nd = inf;
-        for (int i = 0; i < num_BS; i++) {
-            BS_pos.at(i) = coordinate();
-            double dst = cal_dst(origin, BS_pos.at(i));
-            if (dst < nd) {
-                nd = dst; index = i;
-            }
-        }
-        
-        double SI = 0, coef = 0.0;
-        //端末情報を初期化
-        for (int i = 0; i < num_IoT; i++) {
-            pair<double, double> pos;
-            if (i == 0) {
-                pos = origin;
-            } else pos = coordinate();
-            
-            //端末-基地局間のフェージング係数を設定
-            double H = gauss_rand(0, 1);
-            double dst = cal_dst(pos, BS_pos.at(index));
-            if (i == 0) coef = H / pow(dst, alpha);
-            else SI += H / pow(dst, alpha);
-        }
-//        pair<double, double> SI = ini(num_IoT, num_BS, alpha);
-        double SIR = coef / SI;
-        if (SIR > theta) success++;
-        
-        //進捗状況を表示
-//        double progress = i / end_time;
-//        if (progress > 0.8 && p_flag[3]) {cout << "...80%"; p_flag[3] = false;}
-//        else if (progress > 0.6 && p_flag[2]) {cout << "...60%"; p_flag[2] = false;}
-//        else if (progress > 0.4 && p_flag[1]) {cout << "...40%"; p_flag[1] = false;}
-        //else if (progress > 0.2 && p_flag[0]) {cout << "...20%"; p_flag[0] = false;}
-    }
-    //cout << "...100%" << endl;
-    
-    double res = success / end_time;
-    double I = theo(lambda_IoT, lambda_BS, alpha);
-    cout << res << " " << I << endl << endl;
-    outputfile << res << " " << I << " ";
-    //outputfile << res;
-}
-
 void sim_pr2(double lambda_IoT, double alpha) {
     double success = 0;
     cout << "pass loss exp : " << alpha << endl;
@@ -254,7 +198,7 @@ void sim_pr2(double lambda_IoT, double alpha) {
 
 
 void sim_thp(double lambda_IoT, double alpha) {
-    double success = 0.0;
+    double success = 0.0, power_dif_sum = 0.0;
     cout << "pass loss exp : " << alpha << endl;
     
     bool p_flag[4] = {true, true, true, true};
@@ -298,11 +242,18 @@ void sim_thp(double lambda_IoT, double alpha) {
         }
         
         int s = (int)acl.size();
+        double big = 0, big2 = 0;
         for (int i = 0; i < s; i++) {
             double P = device.at(acl.at(i)).coef;
-            double SIR = P / (SI - P);
+            if (big2 < P) {
+                if (big < P) big = P;
+                else big2 = P;
+            }
+            double N = normrand();
+            double SIR = P / (SI - P + N);
             if (SIR > theta) {success++; break;}
         }
+        power_dif_sum += (big - big2);
         
         //進捗状況を表示
         double progress = i / end_time;
@@ -316,32 +267,123 @@ void sim_thp(double lambda_IoT, double alpha) {
     double res = success / end_time;
     double I = thp_theo(lambda_IoT, lambda_BS, alpha);
     cout << "Throughput : " <<  res << " " << I << endl << endl;
-    outputfile << res << " " << I << " ";
+    //outputfile << res << " " << I << " ";
 }
 
 
+//NOMA
+void NOMA_thp(double lambda_IoT, double alpha) {
+    double success = 0.0;
+    cout << "pass loss exp : " << alpha << endl;
+    
+    bool p_flag[4] = {true, true, true, true};
+    cout << "Progress is 0%";
+    for (int i = 0; i < end_time; i++) {
+        int num_IoT = poisson_dist(4 * radius * radius * lambda_IoT);
+        int num_BS = poisson_dist(4 * radius * radius * lambda_BS);
+        //BSの座標を設定
+        vector<pair<double, double>> BS_pos(num_BS);
+        pair<double, double> origin = make_pair(0, 0);
+        BS_pos.at(0) = origin;
+        for (int i = 1; i < num_BS; i++) {
+            BS_pos.at(i) = coordinate();
+        }
+        
+        double SI = 0;
+        vector<terminal> device(num_IoT);
+        vector<int> acl;
+        //端末情報を初期化
+        for (int i = 0; i < num_IoT; i++) {
+            pair<double, double> pos = coordinate();
+            double dst_to = cal_dst(pos, origin);
+            device.at(i).dst_to_origin = dst_to;
+            if (dst_to < 20) {
+                for (int j = 0; j < num_BS; j++) {
+                    double dst = cal_dst(pos, BS_pos.at(j));
+                    device.at(i).state = true;
+                    if (dst < device.at(i).dst_to_origin) {
+                        device.at(i).state = false;
+                        break;
+                    }
+                    if (j == num_BS - 1) acl.push_back(i);
+                }
+            }
+            
+            //端末-基地局間のフェージング係数を設定
+            double H = exp_dist(1.0);//gauss_rand(0, 1);
+            double coef = H / pow(device.at(i).dst_to_origin, alpha);
+            device.at(i).coef = coef;
+            SI += coef;
+        }
+        
+        int s = (int)acl.size();
+        double big = 0, big2 = 0;
+        for (int i = 0; i < s; i++) {
+            double P = device.at(acl.at(i)).coef;
+            if (big2 < P) {
+                if (big < P) big = P;
+                else big2 = P;
+            }
+        }
+        
+        SI -= big;
+        double SINR = big / (SI * 1.1);
+        if (SINR > theta) success++;
+        SI -= big2;
+        SINR = big2 / (SI * 1.1);
+        if (SINR > theta) success++;
+        
+        //進捗状況を表示
+        double progress = i / end_time;
+        if (progress > 0.8 && p_flag[3]) {cout << "...80%"; p_flag[3] = false;}
+        else if (progress > 0.6 && p_flag[2]) {cout << "...60%"; p_flag[2] = false;}
+        else if (progress > 0.4 && p_flag[1]) {cout << "...40%"; p_flag[1] = false;}
+        else if (progress > 0.2 && p_flag[0]) {cout << "...20%"; p_flag[0] = false;}
+    }
+    cout << "...100%" << endl;
+    
+    double res = success / end_time;
+    double I = thp_theo(lambda_IoT, lambda_BS, alpha);
+    cout << "Throughput : " <<  res << " " << I << endl << endl;
+    //outputfile << res << " " << I << " ";
+}
+
+
+
 int main() {
-    cout << "1: Pr sim, 2: Thp sim : ";
+    cout << "1: Pr sim, 2: Thp sim, 3: NOMA : ";
     int key; cin >> key;
     cout << "Put start lambda IoT : ";
     double k; cin >> k; cout << endl;
     string filename;
     if (key == 1) filename = to_string(k) + "pr.txt";
     else filename = to_string(k) + "thp.txt";
-    outputfile.open(filename);
+    //outputfile.open(filename);
     for (double i = k; i <= 5; i += 0.5) {
-        outputfile << i << " ";
+        //outputfile << i << " ";
         cout << "lambda IoT : " << i << endl;
         for (double alpha = 2.5; alpha <= 4.5; alpha += 0.5) {
-//            int num_IoT = poisson_dist(4 * radius * radius * i);
-//            cout << num_IoT << endl;
             if (key == 1) sim_pr2(i, alpha);
-            else sim_thp(i, alpha);
-            //cout << alpha << " " << thp_theo(i, lambda_BS, alpha) << " " << endl;
+            else if (key == 2) sim_thp(i, alpha);
+            else NOMA_thp(i, alpha);
         }
         cout << endl;
-        outputfile << endl;
+        //outputfile << endl;
     }
+    
+    
+    //max throughput
+//    string filename = "max_thp2.txt";
+//    outputfile.open(filename);
+//    double i = 10.;
+//    for (double alpha = 2; alpha <= 10; alpha += 0.5) {
+//        outputfile << alpha << " ";
+//        sim_thp(i, alpha);
+//        //cout << alpha << " " << thp_theo(i, lambda_BS, alpha) << " " << endl;
+//    }
+//    cout << endl;
+//    outputfile << endl;
+    
     
 //    random_device rd;
 //    init_genrand(rd());
@@ -350,4 +392,24 @@ int main() {
 //        cout << test.first << " " << test.second << endl;
 //    }
 //
+    
+    
+//    for (double alpha = 2; alpha <= 10; alpha += 0.5) {
+//        double delta = 2.0 / alpha;
+//        double A = pow(theta, delta) * PI * delta / sin(PI * delta);
+//        cout << alpha << " " << 1/A << endl;
+//        //cout << "alpha = " << alpha << " , A = " << A << ", 1/A = " << 1. / A << endl;
+//    }
+    
+    //A 最大スループット
+//    for (double i = 1; i < 1000000; i+=100) {
+//        double alpha = 4.5;
+//        double delta = 2.0 / alpha;
+//        double A = pow(theta, delta) * PI * delta / sin(PI * delta);
+//        double I = theo(i, lambda_BS, alpha);
+//        double T = thp_theo(i, lambda_BS, alpha);
+//        cout << i << " " << T << " " << I * i << " " << 1. / A << endl;
+//    }
+    
+    
 }
